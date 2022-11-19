788efc0220917fa88ae678a6e67e83b9fe69d271
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-11-19 16:46:00 -0600

    Using the modulus operator

--- Variables/MathOperations.java
+++ Variables/MathOperations.java
@@ -9,20 +9,32 @@ public class MathOperations {
         double testScores = 6.7;
         double bonusMarks = 2.5;
 
+        // The addition operator '+'
+
         System.out.println("Fred and George collected " + (bagOfSweets1 + bagOfSweets2) + " sweets");
         System.out.println("There are " + (starsInMilkyWay + starsInAndromeda) + " stars in the Milky Way and Andromeda galaxies.");
         System.out.println("With bonus marks, Ron scored " + (testScores + bonusMarks) + "/10 on his potions test. Snape wasn't too thrilled");
+
+        //The subtraction operator '-'
 
         System.out.println("Fred has  " + (bagOfSweets2 - bagOfSweets1) + " more sweets than George");
         System.out.println("The Andromeda Galaxy has " + (starsInAndromeda - starsInMilkyWay) + " more stars than the Milky Way");
         System.out.println("Snape ended up removing marks. Ron actually scored " + (testScores - bonusMarks) + " on his potions test");
 
+        // The multiplication operator '*'
+
         System.out.println("Fred and George used a special spell to multiply their sweets to: " + (bagOfSweets1*bagOfSweets2));
         System.out.println("Snape made an error on his excel, accidentally multiplying Ron's score to: " + (testScores*bonusMarks) + "/10");
+
+        // The division operator '/'
 
         System.out.println("Originally, Fred had " + (bagOfSweets2/bagOfSweets1) +" times more sweets than George");
         System.out.println("Andromeda has " + (starsInAndromeda/starsInMilkyWay) + " times more stars");
         System.out.println("Snape caught the error. Furious, he divided Ron's score to: " + (testScores/bonusMarks));
 
+        // The modulus operator '%'
+
+        System.out.println("10 is an even number, since dividing 10 by 2 has a remainder of: " + (10 % 2));
+        System.out.println("5 is an odd number, since dividing 5 by 2 has a remainder of: " + (5 % 2));
     }
 }
