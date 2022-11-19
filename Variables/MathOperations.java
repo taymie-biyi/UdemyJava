42920c5ee99fbdbd69a31c0adf5291121e2df87a
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-11-19 15:50:48 -0600

    Using the addition operator

---
+++ Variables/MathOperations.java
@@ -0,0 +1,17 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        int bagOfSweets1 = 5;
+        int bagOfSweets2 = 10;
+
+        long starsInMilkyWay = 2500000000L;
+        long starsInAndromeda = 1000000000000L;
+
+        double testScores = 6.7;
+        double bonusMarks = 2.5;
+
+        System.out.println("Fred and George collected " + (bagOfSweets1 + bagOfSweets2) + " sweets");
+        System.out.println("There are " + (starsInMilkyWay + starsInAndromeda) + " stars in the Milky Way and Andromeda galaxies.");
+        System.out.println("With bonus marks, Ron scored " + (testScores + bonusMarks) + "/10 on his potions test. Snape wasn't too thrilled");
+
+    }
+}
