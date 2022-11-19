c58849774fa56f1648aad286743873aba18bc596
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-11-19 17:32:10 -0600

    Using the 'Add One' operator, '++'

--- Variables/MathOperations.java
+++ Variables/MathOperations.java
@@ -36,5 +36,12 @@ public class MathOperations {
 
         System.out.println("10 is an even number, since dividing 10 by 2 has a remainder of: " + (10 % 2));
         System.out.println("5 is an odd number, since dividing 5 by 2 has a remainder of: " + (5 % 2));
+
+        //The 'add one' operator, '++'
+
+        int counter = 0;
+        counter++;
+        counter++;
+        System.out.println("I count: " + counter);
     }
 }
