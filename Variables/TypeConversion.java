3a58fc1939374345e73d115567eff18a36e287d8
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-11-20 17:39:54 -0600

    Casting double to int

---
+++ Variables/TypeConversion.java
@@ -0,0 +1,12 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        // Casting double to int 
+        double decimal = 4.3;
+         int integer = (int)decimal;
+
+         double salary = 5833.333333;
+         int roundedSalary = (int)salary;
+
+         System.out.println("Percy's monthly salary is $" + roundedSalary);
+    }
+}
