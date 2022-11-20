e3bc87581a538e940973d8feb20b1b922d1691e9
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-11-20 17:54:43 -0600

    Solve workshop 2.4

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.4/Meeting.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.4/Meeting.java
@@ -4,11 +4,17 @@ public class Meeting {
         double profit = 18562.18; 
         double refunds = 688.78; 
         double shipping = 1233.57; 
+        // convert from double to integers
 
-        System.out.println("This month, we made $<sales> in sales");
-        System.out.println("Factoring in costs, we made $<profit> in profit");
-        System.out.println("The refunds are at a low $<refunds>. This is a good sign!");
-        System.out.println("Shipping costs were high. We paid $<shipping> in shipping");
+        int newSales = (int)sales;
+        int newProfit = (int)profit;
+        int newRefunds = (int)refunds;
+        int newShipping = (int)shipping;
+
+        System.out.println("This month, we made $"+ newSales + " in sales");
+        System.out.println("Factoring in costs, we made $" + newProfit + " in profit");
+        System.out.println("The refunds are at a low $" + newRefunds + ". This is a good sign!");
+        System.out.println("Shipping costs were high. We paid $" + newShipping + " in shipping");
 
 
     }
