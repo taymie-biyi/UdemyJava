a1dcf39d8269a01694ae7cdd04c9ae736553979a
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-11-20 19:59:47 -0600

    Solve Workbook 2.5

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
@@ -11,33 +11,51 @@ public class SignUp {
         //Task 1 - Using Scanner, ask the user questions
 
         //Ask for their first name.
+        System.out.println("\nWhat is your first name?");
+        String firstName = scan.nextLine();
+
         //Ask for their last name.
+        System.out.println("\nWhat is your last name?");
+        String lastName = scan.nextLine();
+
         //Ask: how old are you?
+        System.out.println("\nHow old are you?");
+        int age = scan.nextInt();
+
         /* Ask them to make a username. SIDE NOTE:
            – nextLine() gets "skipped" if you put it after nextInt() , nextDouble(), nextLong().
            – You will understand why this happens when we cover Delimiters in the next section (Booleans and Conditionals).
            – For now, just know that the solution is to add an extra nextLine().
         */
+        String fake = scan.nextLine();
+
+        System.out.println("\nCreate a username");
+        String username = scan.nextLine();
+
         //Ask what city they live in.
+        System.out.println("\nWhat city do you reside?");
+        String city = scan.nextLine();
+
         //Ask what country that's from.
+        System.out.println("\nWhat country do you reside?");
+        String country = scan. nextLine();
 
-
+        
         //Task 2 - Print their information. 
 
-        System.out.println("Thank you for joining JavaGram!");
+        System.out.println("\n\nThank you for joining JavaGram!");
 
         //Print their information like so:
 
-        // Your information:
-        //        First Name: Rayan
-        //        Last Name: Slim
-        //        Age: 27
-        //        Username: monotonic_relu
-        //        City: Ottawa
-        //        Country: Canada
-        //
-
+        System.out.println("\nYour information:");
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("Username: " + username);
+        System.out.println("City: " + city);
+        System.out.println("Country: " + country);
 
         //close scanner. It's good practice :D ! 
+        scan.close();
     }
 }
