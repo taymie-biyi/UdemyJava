92cdc2bcbe032cc5de7003f851e75a8897e1f04f
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-11-26 18:32:25 -0500

    Solve Workbook 3.5

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
@@ -4,8 +4,17 @@ public class WorkSchedule {
         boolean holiday = true;
          
         // if it's a holiday, print: "woohoo, no work!");
-        // if it's the weekend, print: "it's the weekend, no work!"
-        // otherwise, print: "Wake up at 7:00 :( ";
+        if (!holiday) {
+            System.out.println("woohoo, no work!");
         }
+        // if it's the weekend, print: 
+        else if (day == 6 || day == 7) {
+            System.out.println("it's the weekend, no work!");
+        } 
+        // otherwise, print: ;
+        else {
+            System.out.println("Wake up at 7:00 :( ");
+        }
+    }
 
     }
