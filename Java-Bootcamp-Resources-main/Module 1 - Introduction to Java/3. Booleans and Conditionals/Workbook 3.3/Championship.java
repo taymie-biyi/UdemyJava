fde36a9aeecf23766bd97739f09e440235decad1
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-11-26 11:30:49 -0500

    Solve workbook 3.3

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
@@ -1,23 +1,32 @@
 public class Championship {
     public static void main(String[] args) {
 
-        int gryffindor = 400;    //gryffindor points
+        int gryffindor = 300;    //gryffindor points
         int ravenclaw = 200;    //ravenclaw points
 
-        // int margin = amount of points by which gryffindor scored over ravenclaw;
+         int margin = gryffindor - ravenclaw;
 
 
         //If gryffindor wins by a margin of 300 points or more:
-             //print: Gryffindor takes the house cup!
+             //print: 
+        if (margin >= 300) {
+            System.out.println("Gryffindor takes the house cup!");
+        } 
 
         //If gryffindor wins by a margin of any points or ties: 
             //print: In second place, Gryffindor!
-
+            else if (margin >= 0) {
+                System.out.println("In second place, Gryffindor!" + gryffindor);
+            }
         //If gryffindor loses within 100 points, print:
             //print: In third place, Gryffindor!
-
+            else if (margin >= -100) {
+                System.out.println("In third place, Gryffindor!");
+            }
         //else:
            //print: In fourth place, Gryffindor! 
-        
+            else {
+                System.out.println("In fourth place, Gryffindor!");
+            }
     }
 }
