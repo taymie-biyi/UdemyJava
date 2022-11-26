38a83d56a4fdd670a11ce40a5ac9f3ad8a2a037a
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-11-26 11:48:31 -0500

    Solve Workbook 3.4

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
@@ -8,11 +8,20 @@ public class WeatherNetwork {
         //IF - ELSE IF - ELSE STATEMENTS HERE!
 
         //if temp is less than or equal to -1: "The forecast is FREEZING! Stay home!"
-
+        if (temp <= -1) {
+            System.out.println("The forecast is FREEZING! Stay home!");
+            forecast = "FREEZING";
+        }
         //if temp is less than or equal to 10: "The forecast is Chilly. Wear a coat!";
-
-        //else: It's warm. go outside!
-
+            else if (temp <= 10) {
+                System.out.println("The forecast is Chilly. Wear a coat!");
+                forecast = "Chilly";
+            }
+        //else: 
+            else {
+                System.out.println("It's warm. go outside!");
+                forecast = "Warm";
+            }
         System.out.println(forecast);
     }
 }
