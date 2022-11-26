1c4a03a8e8d59e2cbbdc104dd7836479907705fc
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-11-26 11:00:42 -0500

    Using If-Else If-Else Statement

--- Boolean and Conditionals/IfElse.java
+++ Boolean and Conditionals/IfElse.java
@@ -2,10 +2,32 @@ public class IfElse {
     public static void main(String[] args) {
         int grade = 65;
 
+        // If - Else Statement
         if (grade > 65) {
             System.out.println("Great job, you passed!");
         } else {
             System.out.println("Better luck next time!");
+        }
+
+        //If - Else If - Else statement
+        if (grade >= 85) {
+            System.out.println("Excellent! You got an A+.");
+        } else if (grade >= 80) {
+            System.out.println("Excellent! You got an A.");
+        } else if (grade >= 75) {
+            System.out.println("Very Good! You got a B+.");
+        } else if (grade >= 70) {
+            System.out.println("Very Good! You got a B.");
+        } else if (grade >= 65) {
+            System.out.println("Good! You got a C+.");
+        } else if (grade >= 60) {
+            System.out.println("Good! You got a C.");
+        } else if (grade >= 55) {
+            System.out.println("You got a D+.");
+        } else if (grade >= 50) {
+            System.out.println("You got a D.");
+        } else {
+            System.out.println("You got a F. Study hard next time!");
         }
     }
 }
