61fae7c9627c49e2d86050a7e53211faffd9063d
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-11-26 10:14:59 -0500

    Using If Else statement

---
+++ Boolean and Conditionals/IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int grade = 65;
+
+        if (grade > 65) {
+            System.out.println("Great job, you passed!");
+        } else {
+            System.out.println("Better luck next time!");
+        }
+    }
+}
