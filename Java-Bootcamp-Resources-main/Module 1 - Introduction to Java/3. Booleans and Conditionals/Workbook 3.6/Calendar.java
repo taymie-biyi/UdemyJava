4b058456c21a11d7ae0c40b5c01775199d22fd8a
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-11-26 19:03:28 -0500

    Solve Workbook 3.6

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.6/Calendar.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.6/Calendar.java
@@ -7,15 +7,32 @@ public class Calendar {
         System.out.println("Hmm, let me check my calendar.");
         
         //Check calendar here:
-
-            //  "Monday": print ("Sorry, I have to stay at work late."); 
-            //  "Tuesday": print ("It looks like I have meetings all day."); 
-            //  "Wednesday": print ("I have a dentist appointment. Some other time!"); 
-            //  "Thursday": print ("Sorry, thursday is date night!"); 
-            //  "Friday": print ("I'm free!!"); 
+        switch (day) {
+            case "Monday": System.out.println("Sorry, I have to stay at work late.");
+            break;
+            case "Tuesday": System.out.println("It looks like I have meetings all day.");
+            break;
+            case "Wednesday": System.out.println("I have a dentist appointment. Some other time!");
+            break;
+            case "Thursday": System.out.println("Sorry, thursday is date night!");
+            break;
+            case "Friday": System.out.println("I'm free!!");
+            break;
+            case "Saturday": System.out.println("I'm free!!");
+            break;
+            case "Sunday": System.out.println("I'm free!!");
+            break;
+            default: System.out.println("that's not a day.");
+            
+        }
+            //  "Monday": print (); 
+            //  "Tuesday": print (); 
+            //  "Wednesday": print (); 
+            //  "Thursday": print (); 
+            //  "Friday": print (); 
             //  "Saturday": print ("I'm free!!"); 
             //  "Sunday": print ("I'm free!!"); 
-            //  "otherwise": print ("that's not a day."); 
+            //  "otherwise": print (); 
 
         
         
