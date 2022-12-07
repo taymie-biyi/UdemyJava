e39fc83bdef2bd51ed40cdbb474eb1425b129c9d
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-12-06 23:26:56 -0600

    Functions with parameters

---
+++ Functions/Parameters.java
@@ -0,0 +1,10 @@
+public class Parameters {
+    public static void main(String[] args) {
+        calculateArea(2.3, 3.6);
+        calculateArea(1.6, 2.4);
+
+    }
+    public static void calculateArea(double length, double width) {
+        System.out.println("Area: " + length * width);
+    }
+}
