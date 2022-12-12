55fde97524039aad38227010977a66b367ca993b
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-12-11 19:25:46 -0600

    Void Functions vs Value-returning Functions

--- Functions/ReturnValues.java
+++ Functions/ReturnValues.java
@@ -1,19 +1,27 @@
 public class ReturnValues {
     public static void main(String[] args) {
-        calculateArea(2.3, 3.6);
-        calculateArea(1.6, 2.4);
+        // calculateArea(2.3, 3.6);
+        // calculateArea(1.6, 2.4);
+
+        // String englishExplanation = explainArea("English");
+        // String frenchExplanation = explainArea("French");
+        // String spanishExplanation = explainArea("Spanish");
+        // String italianExplanation = explainArea("Italian");
+        double area1 = calculateArea(2.3, 3.6);
+        printArea(2.3, 3.6, area1);
 
         String englishExplanation = explainArea("English");
-        String frenchExplanation = explainArea("French");
-        String spanishExplanation = explainArea("Spanish");
-        String italianExplanation = explainArea("Italian");
-
-
+        System.out.println(englishExplanation);
     }
     public static double calculateArea(double length, double width) {
+        if (length < 0 || width < 0) {
+            System.out.println ("INVALID DIMENSIONS");
+            System.exit(0);
+        }
         double area = length * width;
         return area;
     }
+
     public static String explainArea(String language) {
         switch (language) {
             case "English": return "Area equals length * width";
@@ -21,5 +29,9 @@ public class ReturnValues {
             case "Spanish": return "area es igual a largo * ancho";
             default: return "Language not available";
         }
+    }
+
+    public static void printArea(double length, double width, double area) {
+        System.out.println ("A rectangle with a length of " + length + " and a width of " + width + " has an area of " + area);
     }
 }
