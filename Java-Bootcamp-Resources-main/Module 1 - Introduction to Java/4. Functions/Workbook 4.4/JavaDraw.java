03cfb342f684ebc44e3310738ccb30bf12499932
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-12-11 20:29:45 -0600

    Solve Workbook 4.4

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.4/JavaDraw.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.4/JavaDraw.java
@@ -13,8 +13,11 @@ public class JavaDraw {
         System.out.println( "Write 4 for snake     " );
 
         //Task 1 – Pick up the user's choice.
+        choice = scan.nextInt();
 
         //Task 3 – Call the draw function, and pass in the user's choice.
+        draw(choice);
+
         scan.close();
     }
 
@@ -32,6 +35,85 @@ public class JavaDraw {
      *      3. bear
      *      4. snake
      */  
+
+     public static void draw(int choice) {
+        if (choice == 1){
+                System.out.println("  .==-.                   .-==.     ");
+                System.out.println("   \\()8`-._  `.   .'  _.-'8()/     ");
+                System.out.println("   (88\"   ::.  \\./  .::   \"88)     ");
+                System.out.println("    \\_.'`-::::.(#).::::-'`._/      ");
+                System.out.println("      `._... .q(_)p. ..._.'         ");
+                System.out.println("        \"\"-..-'|=|`-..-\"\"       ");
+                System.out.println("        .\"\"' .'|=|`. `\"\".       ");
+                System.out.println("      ,':8(o)./|=|\\.(o)8:`.        ");
+                System.out.println("     (O :8 ::/ \\_/ \\:: 8: O)      ");
+                System.out.println("      \\O `::/       \\::' O/       ");
+                System.out.println("       \"\"--'         `--\"\"      ");
+        }
+        
+        if (choice == 2){
+                System.out.println("       _..--\"\"-.                  .-\"\"--.._ ");
+                System.out.println("   _.-'         \\ __...----...__ /         '-._");
+                System.out.println(" .'      .:::...,'              ',...:::.      '.");
+                System.out.println("(     .'``'''::;                  ;::'''``'.     )");
+                System.out.println(" \\             '-)              (-'             /");
+                System.out.println("  \\             /                \\             /");
+                System.out.println("   \\          .'.-.            .-.'.          /");
+                System.out.println("    \\         | \\0|            |0/ |         /");
+                System.out.println("    |          \\  |   .-==-.   |  /          |");
+                System.out.println("     \\          `/`;          ;`\\`          /");
+                System.out.println("      '.._      (_ |  .-==-.  | _)      _..'");
+                System.out.println("          `\"`\"-`/ `/'        '\\` \\`-\"`\"`");
+                System.out.println("               / /`;   .==.   ;`\\ \\");
+                System.out.println("         .---./_/   \\  .==.  /   \\ \\");
+                System.out.println("        / '.    `-.__)       |    `\"");
+                System.out.println("       | =(`-.        '==.   ;");
+                System.out.println("        \\  '. `-.           /");
+                System.out.println("         \\_:_)   `\"--.....-'");
+        }
+
+        if (choice == 3) {
+                System.out.println("            ___   .--. ");
+                System.out.println("      .--.-\"   \"-' .- |");
+                System.out.println("     / .-,`          .'");
+                System.out.println("     \\   `           \\");
+                System.out.println("      '.            ! \\");
+                System.out.println("        |     !  .--.  |");
+                System.out.println("        \\        '--'  /.____");
+                System.out.println("       /`-.     \\__,'.'      `\\");
+                System.out.println("    __/   \\`-.____.-' `\\      /");
+                System.out.println("    | `---`'-'._/-`     \\----'    _");
+                System.out.println("    |,-'`  /             |    _.-' `\\");
+                System.out.println("   .'     /              |--'`     / |");
+                System.out.println("  /      /\\              `         | |");
+                System.out.println("  |   .\\/  \\      .--. __          \\ |");
+                System.out.println("   '-'      '._       /  `\\         /");
+                System.out.println("               `\\    '     |------'`");
+                System.out.println("                 \\  |      |");
+                System.out.println("                  \\        /");
+                System.out.println("                   '._  _.'");
+                System.out.println("                      ``");
+                }
+
+        if (choice == 4) {
+                System.out.println("         ,,'6''-,.");
+                System.out.println("        <====,.;;--.");
+                System.out.println("        _`---===. \"\"\"==__");
+                System.out.println("      //\"\"@@-\\===\\@@@@ \"\"\\\\");
+                System.out.println("     |( @@@  |===|  @@@  ||");
+                System.out.println("      \\\\ @@   |===|  @@  //");
+                System.out.println("        \\\\ @@ |===|@@@ //");
+                System.out.println("         \\\\  |===|  //");
+                System.out.println("___________\\\\|===| //_____,----\"\"\"\"\"\"\"\"\"\"-----,_");
+                System.out.println("  \"\"\"\"---,__`\\===`/ _________,---------,____    `,");
+                System.out.println("             |==||                           `\\   \\");
+                System.out.println("            |==| |                             )   |");
+                System.out.println("           |==| |       _____         ______,--'   '");
+                System.out.println("           |=|  `----\"\"\"     `\"\"\"\"\"\"\"\"         _,-'");
+                System.out.println("            `=\\     __,---\"\"\"-------------\"\"\"''");
+                System.out.println("                \"\"\"\"		");
+                }
+        }
 
      /*
      
@@ -120,5 +202,5 @@ public class JavaDraw {
 
      
      */
-
 }
+
