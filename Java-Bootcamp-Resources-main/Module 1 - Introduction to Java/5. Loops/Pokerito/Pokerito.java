4e82bc64e57e14461125c87e882a100a20d87769
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-12-27 19:44:15 -0600

    Complete Pokerito Project

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Pokerito/Pokerito.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Pokerito/Pokerito.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
 
+import javax.sql.rowset.spi.SyncResolver;
+
 public class Pokerito {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Let's play Pokerito. Type anything when you're ready.");
+        scan.nextLine();
+        System.out.println("It's like Poker, but a lot simpler.\n");
+        System.out.println("\n There are two players, you and the computer.");
+        System.out.println("The dealer will give each player one card.");
+        System.out.println("Then, the dealer will draw five cards (the river)");
+        System.out.println("The player with the most river matches wins!");
+        System.out.println("If the matches are equal, everyone's a winner! \n");
+        System.out.println("\nReady? Type anything if you are.");
 
+        scan.nextLine();
         /*Task 2: Explain the rules
 
         >>Let's play Pokerito. Type anything when you're ready.
@@ -19,8 +31,14 @@ public class Pokerito {
         >> • Ready? Type anything if you are.
         |
         */
-        
+        String yourcard = randomCard();
+        String compCard = randomCard();
 
+        System.out.println("Here's your card: ");
+        System.out.println(yourcard);
+
+        System.out.println("\nHere's the computer's card:");
+        System.out.println(compCard);
         /*Task 3: Present the user with a card
          println 'Here's your card:'
          <show card>
@@ -45,7 +63,20 @@ public class Pokerito {
          *      <print card>
          *      ...
          */
-
+        System.out.println("Now, the dealer will draw five cards. Press enter to continue.");
+        System.out.println("The dealer will draw a card every time the user presses enter.");
+        for (int i=1; i <= 5; i++){
+            scan.nextLine();
+            String draw = randomCard();
+            System.out.println("Card " + i);
+            System.out.println(draw);
+            if (draw.equals(yourcard)){
+                yourMatches++;
+            } else if (draw.equals(compCard)) {
+                computerMatches++;
+            }
+        }
+    
         /** Task 5 - Get the winner
          * 
          * • Count your number of matches.
@@ -57,8 +88,12 @@ public class Pokerito {
          * • If the computer has more matches, print: The computer wins! 
          * • If the matches are equal, print: everyone wins!.
          */
-
+    
+        System.out.println("Your number of matches: " + yourMatches);
+        System.out.println("Computer number of matches: " + computerMatches);
+        
          scan.close();
+        
     }
 
     /** Task 1
@@ -70,5 +105,126 @@ public class Pokerito {
      *   1. Gets a random number between 1 and 13.
      *   2. Returns a card that matches the random number (get the String values from cards.text).   
      */
-
+    public static String randomCard() {
+        int randomNum = (int) (Math.random() * 13) +1;
+        switch (randomNum) {
+            case 1:
+            return (
+                "   _____\n"+
+                "  |A _  |\n"+ 
+                "  | ( ) |\n"+
+                "  |(_'_)|\n"+
+                "  |  |  |\n"+
+                "  |____V|\n"
+            );
+            case 2:
+                return (
+                "   _____\n"+              
+                "  |2    |\n"+ 
+                "  |  o  |\n"+
+                "  |     |\n"+
+                "  |  o  |\n"+
+                "  |____Z|\n"
+                );
+            case 3:
+                return (
+                  "   _____\n" +
+                  "  |3    |\n"+
+                  "  | o o |\n"+
+                  "  |     |\n"+
+                  "  |  o  |\n"+
+                  "  |____E|\n"
+                );
+            case 4:
+                return (
+                   "   _____\n" +
+                   "  |4    |\n"+
+                   "  | o o |\n"+
+                   "  |     |\n"+
+                   "  | o o |\n"+
+                   "  |____h|\n"
+                );
+            case 5:
+                return (
+                    "   _____ \n" +
+                    "  |5    |\n" +
+                    "  | o o |\n" +
+                    "  |  o  |\n" +
+                    "  | o o |\n" +
+                    "  |____S|\n"
+                );
+            case 6:
+                return (
+                    "   _____ \n" +
+                    "  |6    |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  |____6|\n"
+                );
+            case 7:
+                return (
+                    "   _____ \n" +
+                    "  |7    |\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |____7|\n"
+                );
+            case 8:
+                return (
+                    "   _____ \n" +
+                    "  |8    |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  |____8|\n"
+                );
+            case 9:
+                return (
+                    "   _____ \n" +
+                    "  |9    |\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |____9|\n"
+                );
+            case 10:
+                return (
+                    "   _____ \n" +
+                    "  |10  o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |___10|\n"
+                );
+            case 11:
+                return (
+                    "   _____\n" +
+                    "  |J  ww|\n"+ 
+                    "  | o {)|\n"+ 
+                    "  |o o% |\n"+ 
+                    "  | | % |\n"+ 
+                    "  |__%%[|\n"
+                );
+            case 12:
+                return (
+                    "   _____\n" +
+                    "  |Q  ww|\n"+ 
+                    "  | o {(|\n"+ 
+                    "  |o o%%|\n"+ 
+                    "  | |%%%|\n"+ 
+                    "  |_%%%O|\n"
+                );
+            default: 
+                return (
+                     "  _____\n"+
+                    "  |K  WW|\n"+ 
+                    "  | o {)|\n"+ 
+                    "  |o o%%|\n"+ 
+                    "  | |%%%|\n"+ 
+                    "  |_%%%>|\n"
+                );
+        }
+    }
 }
