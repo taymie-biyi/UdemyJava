08deaa989da6418fba2a12eb7b43726601717874
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-12-29 11:51:45 -0600

    Intro to Arrays

---
+++ Arrays/IntroToArrays.java
@@ -0,0 +1,15 @@
+public class IntroToArrays {
+
+    public static void main(String[] args) {
+        int[] integers = {1, 2, 3};
+
+        String[] words = {"Jane", "James", "John", "Joe"};
+
+        double[] decimals = {1.2, 2.2, 3.3};
+
+        char[] letters ={'b', 'y', 'e'};
+        
+        String[] kingdoms = {"Mercia", "Wessex", "Northumbria"};
+        System.out.println(kingdoms[2]);
+    }
+}
