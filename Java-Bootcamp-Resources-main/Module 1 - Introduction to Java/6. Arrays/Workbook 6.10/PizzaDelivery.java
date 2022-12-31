34f3df8b70c5bd9f729cfae1605606b169660e50
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-12-31 05:17:26 -0600

    Complete Workbook 6.10

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.10/PizzaDelivery.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.10/PizzaDelivery.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PizzaDelivery {
@@ -7,33 +8,47 @@ public class PizzaDelivery {
          *   1. Ask the user: How many pizza toppings do you want?. 
          *   2. Then, pick up the result using Scanner.
          */
-
+        System.out.println("How many pizza toppings do you want?");
+        int result = scan.nextInt();
+        scan.nextLine();
         // Task 2 – Create the array here
-
+        String[] toppings = new String[result];
         /** Task 3
          *  print Great, enter each topping!
          *  Create a for loop that runs through the length of the array.   
          * 
          */
-
+        System.out.println("\nGreat, enter each topping!");
+        
+        for (int i = 0; i < toppings.length; i++){
+            System.out.print(i + ". ");
+            toppings[i] = scan.nextLine();
+        }
         /** Task 4 – Pick up the user's toppings and store them in the array.
          *  
          *  See the workbook article for more detail  
          * 
          */
-
+        
+        
         /** Task 5 –  Loop through the length of the array and print each topping
          *  
          *  See the workbook article for more detail   
          * 
          */
-
+        System.out.println("\nThank you! Here are the toppings you ordered: ");
+        for (int i = 0; i < toppings.length; i++){
+            System.out.println(i + ". " + toppings[i]);
+        }
         /** Task 6 –  Confirm the order
          *  
          *  See the workbook article for more detail  
          * 
          */
+        System.out.println("\nPress anything to confirm your order.");
+        scan.nextLine();
 
+        System.out.println("Great, A driver is on the way!");
         scan.close();
 
 
