6cc81c2d56be7e92d42197cb2b2518beafd328f8
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-12-31 03:15:12 -0600

    Complete Workbook6.5

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.5/HighScore.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.5/HighScore.java
@@ -1,12 +1,18 @@
 public class HighScore {
     public static void main(String[] args) {
-        
+
         /** Task 2 
          *
          *    1. Create an array that stores 10 random numbers
          *    2. Using a for loop, print the array elements on the same line.
          *         Example – Here are the scores: 14775 48328 7928 27102 21787 21063 38096 42711 32863 23707
          */
+        int[] numbers = {randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber()};
+        System.out.println("Here are the scores: ");
+        for (int i = 0; i < numbers.length; i++){
+            System.out.print(numbers[i] + " ");
+        }
+
         int highScore = 0;
 
          /** Task 3
@@ -15,8 +21,12 @@ public class HighScore {
           *   2. println("\n\nThe highest score is: <>. Give that man a cookie!");
 
           */
-
-
+          for (int i = 0; i < numbers.length; i++){
+            if (numbers[i] > highScore) {
+                highScore = numbers[i];
+            }
+          }
+          System.out.println("\nThe highest score is: " + highScore + ". Give that man a cookie");
     }
     /** Task 1
      * Function name – randomNumber
@@ -25,6 +35,9 @@ public class HighScore {
      * Inside the function
      *  - returns a random number between 0 and 50000
      */
-    
+        public static int randomNumber(){
+            int random = (int) (Math.random() * 50001);
+            return (random);
+        }
 
 }
