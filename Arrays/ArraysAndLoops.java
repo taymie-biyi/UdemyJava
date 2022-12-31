059787281e278eb795d7dd86da972e52388e2121
Temi Olubiyi <temiloluwa.olubiyi@yahoo.com>
2022-12-31 02:13:57 -0600

    Arrays and Loops

---
+++ Arrays/ArraysAndLoops.java
@@ -0,0 +1,8 @@
+public class ArraysAndLoops {
+    public static void main(String[] args) {
+        String[] kingdoms = {"Mercia", "Wessex", "Northumbria", "E A"};
+        for (int i = 0; i < kingdoms.length; i++){
+            System.out.println("The elements at index " + i + " is " + kingdoms[i]);
+        }
+    }
+}
