c6ce3d333d1d393f6a5c88f42ce8ed8271dd682e
taymie-biyi <temiloluwa.olubiyi@yahoo.com>
2023-01-02 23:59:34 -0600

    Complete Project TicTacToe

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
@@ -1,29 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToe {
 
     static Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {
-        
-            System.out.println("\nLet's play tic tac toe");
+    public static void main(String[] args) {        
+      System.out.println("\nLet's play tic tac toe");
+      //Task 1: Create an array with three rows of '_' characters.
+      char[][] board = {
+        {'_', '_', '_'},
+        {'_', '_', '_'},
+        {'_', '_', '_'}
+      };
+      //Task 2: Call the function printBoard();
+      printBoard(board);
+      /*
+      {  Task 3: Loop through turns.
 
-            //Task 1: Create an array with three rows of '_' characters.
-
-            //Task 2: Call the function printBoard();
-
-             
-              /*
-              {  Task 3: Loop through turns.
-
-                  if (X) turn {
-                     Task 4: call askUser(). 
-                     Task 5: populate the board using askUser's return value.
-                  } else {
-                      Task 4: call askUser(). 
-                      Task 5: populate the board using askUser's return value. Then, print it.
+      if (X) turn {
+     Task 4: call askUser(). 
+     Task 5: populate the board using askUser's return value.
+      } else {
+      Task 4: call askUser(). 
+      Task 5: populate the board using askUser's return value. Then, print it.
 
                   }
-
+                  
                 Task 6 - Call the function.
                    if return value == 3 {
                      print: X wins and break the loop
@@ -33,6 +35,30 @@ public class TicTacToe {
 
               } 
               */
+              for (int i = 0; i < 9; i++) {
+                System.out.print("Turn: ");
+                if (i % 2 == 0){
+                  System.out.println("X");
+                  int[] spot = askUser(board);
+                  board[spot[0]][spot[1]] = 'X';
+                  printBoard(board);
+                } else {
+                  System.out.println("O");
+                  int[] spot = askUser(board);
+                  board[spot[0]][spot[1]] = 'O';
+                  printBoard(board);
+                }
+                
+                if (checkWin(board) == 3) {
+                  System.out.println("X wins!!");
+                  break;
+                } else if (checkWin(board) == -3) {
+                  System.out.println("O wins!!");
+                  break;
+                } else if (checkWin(board) == 8) {
+                  System.out.println("It's a tie!!");
+                }
+              }
 
             scan.close();
         }
@@ -48,8 +74,16 @@ public class TicTacToe {
      *      • separate each row by two lines. 
      *      • each row precedes a tab of space
      *      • each character in the grid has one space from the other character
-     */        
-
+     */     
+        public static void printBoard(char[][] board) {
+          for (int i = 0; i < board.length; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < board[i].length; j++) {
+              System.out.print(board[i][j] + " ");
+            }
+            System.out.print("\n");
+          }
+        }
    /** Task 4 - Write a function that lets the user choose a spot
      * Function name – askUser
      * @param board (char[][] board)
@@ -61,7 +95,20 @@ public class TicTacToe {
      *   3. Return the row and column in an int[] array.
      * 
      */
+        private static int[] askUser(char[][] board) {
+          System.out.println("pick a row and column number:");
+          int row = scan.nextInt();
+          int col = scan.nextInt();
 
+          if (board[row][col] != '_'){
+            System.out.println("Spot taken! Please try again");
+            row = scan.nextInt();
+            col = scan.nextInt();
+          }
+
+          int[] spot = {row, col};
+          return spot;
+        }
     /** Task 6 - Write a function that determines the winner
      * Function name - checkWin 
      * @param board (char[][])
@@ -74,6 +121,64 @@ public class TicTacToe {
      *   4. Check the left diagonal for a straight X or straight O (Task 9).
      *   5. Check the right diagonal for a straight X or straight O (Task 10).
      */
+        public static int checkWin(char[][] board) {
+          int count = 0;
+          for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+              if (board[i][j] == 'X') {
+                 count++;
+              } else if (board[i][j] == 'O'){
+                 count--;
+              }
+            }
+            if ((count == 3) || (count == -3)){
+              return count;
+            } else {
+              count = 0;
+            }
+          }
 
+          for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+              if (board[j][i] == 'X') {
+                 count++;
+              } else if (board[j][i] == 'O'){
+                 count--;
+              }
+            }
+            if ((count == 3) || (count == -3)){
+              return count;
+            } else {
+              count = 0;
+            }
+          }
 
+          for (int i = 0; i < 3; i++) {
+            if (board[i][i] == 'X') {
+              count++;
+            } else if (board[i][i] == 'O'){
+              count--;
+            }
+          }
+          if ((count == 3) || (count == -3)){
+            return count;
+          } else {
+            count = 0;
+          }
+
+          for (int i = 0; i < 3; i++) {
+            if (board[i][2-i] == 'X') {
+              count++;
+            } else if (board[i][i] == 'O'){
+              count--;
+            }
+          }
+          if ((count == 3) || (count == -3)){
+            return count;
+          } else {
+             count = 0;
+          }
+
+          return count;
+        }
 }
