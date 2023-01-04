9efc8140f630a3cf6502686f48795e8789efeef2
taymie-biyi <temiloluwa.olubiyi@yahoo.com>
2023-01-03 19:24:17 -0600

    Complete airline-resevation-system

--- Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
+++ Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
@@ -1,11 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
   
     public static void main(String[] args) {
+        Person person = new Person();
+        person.name = "Rita";
+        person.nationality = "South African";
+        person.dateOfBirth = "12/12/1987";
+        person.passport = new String[] {person.name, person.nationality, person.dateOfBirth};
+        person.seatNumber = 4;
 
-
-      
-      
-      
+        System.out.println(person.name);
+        System.out.println(person.nationality);
+        System.out.println(person.dateOfBirth);
+        System.out.println(Arrays.toString(person.passport));
+        System.out.println(person.seatNumber);
+        
+        
+        
+        
     }
   
   
